0a4a367c62222e0b6079e41d9fe78041a97aeddc
yihao61 <2601548@sit.singaporetech.edu.sg>
2026-09-11 13:54:00 +0800

    Step 8

--- auditor.py
+++ auditor.py
@@ -1,18 +1,30 @@
 inventory = 0
+invEntry = 0
+
+def Report():
+    print("Printing Report....")
+    print("Total Units Processed: ", inventory)
+    print("Number of Failed/Rejected Entries: ", invEntry)
+    return
+
 
 while 1:
     userInput = input("Please Enter stock quantity: ")
 
     if userInput.lower() == "quit":
+        Report()
+        print("Exiting Program....")
         break
 
     if not userInput.isdigit():
         print("Invalid number, please try again!")
+        invEntry += 1
         continue
     
     inventory += int(userInput)
     print("Inventory Updated! Current inventory: ", inventory)
 
     if inventory > 500:
+        Report()
         print("Overstock detected! Teminating Program...")
         break
