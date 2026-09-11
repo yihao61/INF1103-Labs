4fafe0bfc7983d526b235a8e1dd308be69dd2d17
yihao61 <2601548@sit.singaporetech.edu.sg>
2026-09-11 13:45:23 +0800

    Step 4 & 5

--- auditor.py
+++ auditor.py
@@ -5,6 +5,10 @@ while 1:
 
     if userInput.lower() == "quit":
         break
+
+    if not userInput.isdigit():
+        print("Invalid number, please try again!")
+        continue
     
     inventory += int(userInput)
     print("Inventory Updated! Current inventory: ", inventory)
